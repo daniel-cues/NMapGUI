92017444c90821770875eab86ac25c10e475c039
Daniel Cuesta <danicuestasuarez@gmail.com>
2017-05-05 10:18:17 -0400

    Normalized JS strings

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -9,9 +9,9 @@ $(function() {
 				$(".sidebar-button").removeClass("selected");
 				// add class to the one we clicked
 				$(this).addClass("selected");
-				$('.off-canvas').not(
-						document.getElementById($(this).attr('data-toggle')))
-						.foundation('close');
+				$(".off-canvas").not(
+						document.getElementById($(this).attr("data-toggle")))
+						.foundation("close");
 
 			});
 });
@@ -20,8 +20,8 @@ function performPost() {
 	/*If backend tells the app that command is finished, stops executing*/
 	/* Updates output from backend */
 	var settings_end = {
-		type : 'GET',
-		url : '/nmap/update-finished'
+		type : "GET",
+		url : "/nmap/update-finished"
 	};
 	/* And writes it on its place */
 	$.ajax(settings_end).done(function(result) {
@@ -31,12 +31,12 @@ function performPost() {
 
 	/* Updates output from backend */
 	var settings = {
-		type : 'GET',
-		url : '/nmap/update'
+		type : "GET",
+		url : "/nmap/update"
 	};
 	/* And writes it on its place */
 	$.ajax(settings).done(function(result) {
-		$('#out-container').html(result);
+		$("#out-container").html(result);
 	});
 	
 	
@@ -48,17 +48,17 @@ var loop;
 function startLoop() {
 
 	/* Get Url to send the command */
-	var urlvar = '/nmap-exe';
+	var urlvar = "/nmap-exe";
 
-	if (document.getElementById('command').value != '') {
-		urlvar = urlvar + '?code=' + document.getElementById('command').value;
+	if (document.getElementById("command").value != "") {
+		urlvar = urlvar + "?code=" + document.getElementById("command").value;
 	}
 	/* Execute ajax command request */
 	var settings = {
-		type : 'GET',
+		type : "GET",
 		url : urlvar
 	};
-	$.ajax(settings)
+	$.ajax(settings);
 	/* Stops any current output refreshing and starts live feed from console */
 	if (loop != null)
 		clearInterval(loop);
